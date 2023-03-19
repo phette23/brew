76344a62286baa60f2fb5c189d7157a4d7d720ca
Eric Phetteplace <phette23@gmail.com>
2023-03-19 10:43:11 -0700

    Use fish_add_path in shellenv
    fish_add_path is the recommended way to modify the path in fish shell and avoids creating duplicate entries in the PATH, e.g. if you already have /usr/local/bin in your PATH and then source `brew shellenv`

--- Library/Homebrew/cmd/shellenv.sh
+++ Library/Homebrew/cmd/shellenv.sh
@@ -23,7 +23,7 @@ homebrew-shellenv() {
       echo "set -gx HOMEBREW_PREFIX \"${HOMEBREW_PREFIX}\";"
       echo "set -gx HOMEBREW_CELLAR \"${HOMEBREW_CELLAR}\";"
       echo "set -gx HOMEBREW_REPOSITORY \"${HOMEBREW_REPOSITORY}\";"
-      echo "set -q PATH; or set PATH ''; set -gx PATH \"${HOMEBREW_PREFIX}/bin\" \"${HOMEBREW_PREFIX}/sbin\" \$PATH;"
+      echo "set -q PATH; or set PATH ''; fish_add_path -g \"${HOMEBREW_PREFIX}/bin\" \"${HOMEBREW_PREFIX}/sbin\"
       echo "set -q MANPATH; or set MANPATH ''; set -gx MANPATH \"${HOMEBREW_PREFIX}/share/man\" \$MANPATH;"
       echo "set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH \"${HOMEBREW_PREFIX}/share/info\" \$INFOPATH;"
       ;;
